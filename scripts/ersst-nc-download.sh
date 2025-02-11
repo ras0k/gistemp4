af7f3919421197b48813e046728430a05504a18f
rasok <zachariedostaler@hotmail.com>
2025-02-11 13:27:54 -0500

    Update ersst-nc-download.sh

--- scripts/ersst-nc-download.sh
+++ scripts/ersst-nc-download.sh
@@ -3,7 +3,7 @@
 ersst_url=https://www.ncei.noaa.gov/pub/data/cmb/ersst/v5/netcdf
 #start year and end year to download
 yr_start=2024
-yr_end=(date +%Y)
+yr_end=$(date +%Y)
 
 for iy in $(seq $yr_start $yr_end)
 do
