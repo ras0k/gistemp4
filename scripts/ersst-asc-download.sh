64ea6bbaa492fab204643bed365accaff23c5f78
rasok <zachariedostaler@hotmail.com>
2025-02-11 14:00:03 -0500

    Update ersst-asc-download.sh

--- scripts/ersst-asc-download.sh
+++ scripts/ersst-asc-download.sh
@@ -1,8 +1,7 @@
+#!/bin/sh
 yr=2008
 while [ $yr -le 2025 ]; do
-  nm=1
+  echo "Downloading file for year $yr"
   wget https://www.ncei.noaa.gov/pub/data/cmb/ersst/v5/ascii/ersst.v5.$yr.asc
-  nm=`expr $nm + 1`
-  done
   yr=`expr $yr + 1`
 done
